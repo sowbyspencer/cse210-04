d8371f484171eca7c4c1d763bc0c305345062ece
sowbyspencer@gmail.com <33128124+spencer-sowby-owatc@users.noreply.github.com>
2022-11-25 20:52:33 -0700

    created

---
+++ greed/greed/game/casting/artifact.py
@@ -0,0 +1,29 @@
+from game.casting.actor import Actor
+
+# TODO: Implement the Artifact class here. Don't forget to inherit from Actor!
+class Artifact(Actor):
+    """A subclass inherited from Actor
+
+    Attributes:
+        _message (string): The text to display when found by the robot.
+    """
+
+    def __init__(self):
+        """Constructs a new Artifact"""
+        super().__init__()
+
+    def calculate_points(self): 
+        
+        points = 0
+
+        # Checking the text of the artifact and assigning points based on the text.
+        if (self.get_text() == '*'):
+            points = 1
+        elif (self.get_text() == '^'):
+            points = 2
+        elif (self.get_text() == 'X'):
+            points = -3
+        else:
+            points = -1
+        
+        return points
